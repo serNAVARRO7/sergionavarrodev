ce464172c2510841e7b0e88cfe78ef03493326ae
serNAVARRO <sernavarrops3@gmail.com>
2021-08-01 20:23:31 +0200

    Add buttons grow animation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,8 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
-    initialNavigation: 'enabled'
+    initialNavigation: 'enabled',
+    scrollPositionRestoration: 'enabled'
 })],
   exports: [RouterModule]
 })
